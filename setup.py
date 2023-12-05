f13ac9be0ef711c1aa05f570aaac4bf325cbf991
Amanda Wong <wongamanda@uchicago.edu>
2023-12-05 15:38:15 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='langchain',
     version='0.1',
-    packages=['prompts', 'chains', 'chat_models', 'evaluation', 'tools', 'llms', 'document_loaders'],
-    include_package_data=True,
-    package_dir={'': 'libs/langchain/langchain'},
+    packages=find_packages('libs/langchain/langchain'),
+    # include_package_data=True,
+    # package_dir={'': 'libs/langchain/langchain'},
 )
